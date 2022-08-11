db71579e89fe026731e13a9b52adc13e5aa47a42
d4rkc0de <abdenaceur.lichiheb@gmail.com>
2022-08-11 19:02:12 +0200

    first commit

---
+++ src/main/scala/README.md
@@ -0,0 +1,6 @@
+# Spark-NLP Scala
+
+## Scala
+
+Our package is deployed to maven central. In order to add this package as a dependency in your application please check the [Scala documentation](https://github.com/JohnSnowLabs/spark-nlp#scala).
+
--- src/main/scala/org/d4rkc0de/helper/Utils.scala
+++
@@ -1,9 +0,0 @@
-package org.d4rkc0de.helper
-import scala.reflect.runtime.universe._
-
-object Utils {
-  def getMethods[T: TypeTag]: List[String] =
-    typeOf[T].members.sorted.collect {
-      case m: MethodSymbol if m.isCaseAccessor => m.name.toString
-    }
-}
