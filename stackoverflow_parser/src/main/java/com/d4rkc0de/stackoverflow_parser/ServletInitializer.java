b14b444833cfdf52c4b87679f7361b8e23e69256
d4rkc0de <abdenaceur.lichiheb@gmail.com>
2023-01-08 00:48:30 +0100

    First commit

---
+++ stackoverflow_parser/src/main/java/com/d4rkc0de/stackoverflow_parser/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.d4rkc0de.stackoverflow_parser;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StackoverflowParserApplication.class);
+	}
+
+}
---
+++ stackoverflow_parser/src/main/java/com/d4rkc0de/stackoverflow_parser/StackoverflowParserApplication.java
@@ -0,0 +1,13 @@
+package com.d4rkc0de.stackoverflow_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowParserApplication.class, args);
+	}
+
+}
